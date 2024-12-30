00f72390ea59663dc06bcab6da43258b20c32cb0
Jordan Junaidi <jordanrjunaidi@gmail.com>
2024-12-30 13:55:26 -0800

    Implement part 1.1

--- frontend/src/api/tasks.ts
+++ frontend/src/api/tasks.ts
@@ -94,3 +94,18 @@ export async function getTask(id: string): Promise<APIResult<Task>> {
     return handleAPIError(error);
   }
 }
+
+export async function getAllTasks(): Promise<APIResult<Task[]>> {
+  try {
+    const response = await get("/api/tasks");
+    const json = await response.json();
+
+    for (let i = 0; i < json.length; i++) {
+      parseTask(json[i]);
+    }
+
+    return { success: true, data: json };
+  } catch (error) {
+    return handleAPIError(error);
+  }
+}
